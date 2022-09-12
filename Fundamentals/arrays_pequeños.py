f14555a3752383851f9740bc0e221e54e50a3f74
DavidHC11 <DavidHC11>
2022-09-12 12:43:41 +0000

    arrays tarea

---
+++ Fundamentals/arrays_pequeños.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+import numpy as np
+
+# COMMAND ----------
+
+matriz = np.array(((0,1,2),(3,4,5),(6,7,8)))
+
+print(matriz)
+
+# COMMAND ----------
+
+identidad = np.identity(4)
+
+print(identidad)
+
+# COMMAND ----------
+
+
