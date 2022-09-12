4d2785937fdfbbbc9ac95c464ea38c81e049622a
DavidHC11 <DavidHC11>
2022-09-12 12:37:21 +0000

    edad operaciones

---
+++ Fundamentals/edad.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+a = 24
+b = y/2
+
+print("tengo ",x , "años")
+
+# COMMAND ----------
+
+y = 6
+x = (6+15)*3
+
+print("Tiene ", x, "años")
+
+# COMMAND ----------
+
+print("La persona más grande tiene ",max([a,b,x,y]),"años")
+
+# COMMAND ----------
+
+
