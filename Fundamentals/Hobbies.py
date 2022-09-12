8b2c2f32b0366827db8b41fceef2928167713c8c
DavidHC11 <DavidHC11>
2022-09-12 13:45:35 +0000

    hobbies

---
+++ Fundamentals/Hobbies.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC # Algoritmo para tocar el teclado/piano
+# MAGIC 
+# MAGIC 
+# MAGIC 1. Enchufar o verificar que este enchufado el teclado/piano 
+# MAGIC 2. Encender el teclado/piano
+# MAGIC 3. Realizar ejercicios de calentamiento
+# MAGIC 4. Abrir partituras
+# MAGIC 5. Hacer ejercicios de velocidad
+# MAGIC 6. Hacer ejercicios de tempo
+# MAGIC 7. Practicar canciones
+# MAGIC 8. Cerrar partituras
+# MAGIC 9. Hacer ejercicios de enfriamiento
+# MAGIC 10. Apagar el teclado/piano
+
+# COMMAND ----------
+
+
