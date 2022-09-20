a228f14353208fa03dfdb6d173ae809cad3b5a61
David Hernández C <67530511+DavidHC11@users.noreply.github.com>
2022-09-20 10:47:31 -0500

    mejoras en compras

--- Fundamentals/compras.py
+++ Fundamentals/compras.py
@@ -1,2 +1,11 @@
 # Databricks notebook source
 
+montos = []
+boton = True
+
+while boton:
+    monto = float(input("Ingresa el monto o coloca 0 para finalizar"))
+    if monto == 0:
+        boton = False
+    else:
+        montos.append(monto)
