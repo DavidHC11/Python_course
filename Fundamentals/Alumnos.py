508bed87e917e3278001431decc50542b4467786
DavidHC11 <DavidHC11>
2022-09-30 02:45:05 +0000

    notebook alumnos

--- Fundamentals/Alumnos.py
+++ Fundamentals/Alumnos.py
@@ -17,11 +17,48 @@ df[(df["reading_score"]<1) | (df["math_score"]<1)]
 # COMMAND ----------
 
 resumen = df.groupby(["grade","gender"]).agg({"Student ID":"count","reading_score":"mean","math_score":"mean"}).reset_index()
-sns.barplot(data=df, x="grade", y="Student ID", hue="gender")
+sns.barplot(data=resumen, x="grade", y="Student ID", hue="gender")
 
 # COMMAND ----------
 
 resumen
+
+# COMMAND ----------
+
+# MAGIC %md ## Feature Engineering
+
+# COMMAND ----------
+
+def transform(x):
+    if x < 50:
+        return "reprobado"
+    elif x<70:
+        return "regular"
+    elif x>90:
+        return "bueno"
+    else:
+        return "excelente"
+
+# COMMAND ----------
+
+df["transform_reading_score"] = df["reading_score"].apply(transform)
+df["transform_math_score"] = df["math_score"].apply(transform)
+
+# COMMAND ----------
+
+df
+
+# COMMAND ----------
+
+calificaciones = df.groupby(["gender","transform_reading_score","transform_math_score"]).agg({"Student ID":"count"}).reset_index()
+
+# COMMAND ----------
+
+sns.barplot(data=calificaciones, x="transform_reading_score", y="Student ID", hue="gender")
+
+# COMMAND ----------
+
+sns.barplot(data=calificaciones, x="transform_math_score", y="Student ID", hue="gender")
 
 # COMMAND ----------
 
